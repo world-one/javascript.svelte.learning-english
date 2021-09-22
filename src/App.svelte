f203da38ee4e3c04318f8d65a17e07107ccb8667
world-one <worldone.dev@gmail.com>
2021-09-22 23:04:17 +0900

    add style, word

--- src/App.svelte
+++ src/App.svelte
@@ -1,3 +1,6 @@
+<svelte:head>
+	<title>영단어장</title>
+</svelte:head>
 
 <div class="wrap">
   <h1 class="title">{title}</h1>
@@ -5,19 +8,25 @@
     {#each words as item, index}
       <li class="word"> 
         <span class="word__ko">{item[0]}</span>
-        <span class="word__en">
-          <input class="" type="text" on:input={(e) => isMatched(e.target.value, item[1], index)}>
-        </span>
-        <span>{corrects[index] ? '정답' : '오답'}</span>
+				<div class="word__enter">
+					<span class="word__en">
+						<input class="" type="text" on:input={(e) => isMatched(e.target.value, item[1], index)}>
+					</span>
+					<span>{corrects[index] ? '정답' : '오답'}</span>
+			</div>
       </li>
     {/each}
   </ul>
 </div>
 
 <script>
-  const title = '단어';
+  const title = '단어장';
   const words = [
-    ['~을 존중하다', 'respect']
+    ['~을 존중하다', 'respect'],
+		['의지', 'will'],
+		['개인, 개인의', 'individual'],
+		['편하게 생각해', 'Take it easy'],
+		['~(관계 등)을 유지하다', 'maintain']
   ];
   const corrects = [];
   function isMatched(response, answer, index) {
@@ -29,21 +38,48 @@
 </script>
 
 <style lang="scss">
+	ul, input {
+		margin: 0;
+		padding: 0;
+	}
+	li {
+		list-style: none;
+	}
   .wrap {
     max-width: 786px;
     margin: 0 auto;
     border: 1px solid #aaa;
     border-radius: 8px;
     padding: 8px 12px;
-  }
-	.title {
+		.title {
       border-bottom: 1px solid #f0f0f0;
     }
-  .word {
-    display: flex;
-    justify-content: space-around;
-    font-size: 18px;
   }
+	
+  .word {
+    font-size: 14px;
+		align-items: center;
+		border-bottom: 1px dashed #acacac;
+		margin-bottom: 12px;
+		padding: 0 8px 8px;
+
+  }
+	.word__ko {
+		
+	}
+	.word__enter {
+		display: flex;
+    justify-content: flex-end;
+	}
   .word__en {
+		input {
+			text-align: right;
+			border: 0;
+			border-bottom: 1px solid #666;
+			color: #333;
+			outline: none;
+			padding: 0 4px;
+			margin-right: 16px;
+		}
   }
 </style>
