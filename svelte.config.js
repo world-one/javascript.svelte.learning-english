2b9f6444654d511f697f11e18776c39f8696ab0d
world-one <worldone.dev@gmail.com>
2021-09-24 21:56:07 +0900

    remove svelt config

--- svelte.config.js
+++ svelte.config.js
@@ -1,21 +1,9 @@
-// import preprocess from 'svelte-preprocess';
+import preprocess from 'svelte-preprocess';
 
-// const config = {
-// 	// Consult https://github.com/sveltejs/svelte-preprocess
-// 	// for more information about preprocessors
-// 	preprocess: preprocess(),
-// };
-
-// export default config;
-
-
-module.exports = {
-  preprocess: {
-     style: async ({content, attributes}) => {
-        if (attributes.type !== 'text/postcss') return;
-        return new Promise((resolve, reject) => {
-           resolve({code: '', map: ''});
-        });
-     },
-  },
+const config = {
+	// Consult https://github.com/sveltejs/svelte-preprocess
+	// for more information about preprocessors
+	preprocess: preprocess(),
 };
+
+export default config;
