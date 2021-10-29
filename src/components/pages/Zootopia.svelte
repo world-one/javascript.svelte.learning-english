148cc0a04f019448e4f3119df64f1ad64278df49
world-one <worldone.dev@gmail.com>
2021-10-29 23:36:46 +0900

    add pagination of zootopia script

--- src/components/pages/Zootopia.svelte
+++ src/components/pages/Zootopia.svelte
@@ -1,26 +1,36 @@
 <svelte:head>
-  <title>영문장</title>
+  <title>zootopia</title>
 </svelte:head>
 
 <div class="wrap">
   <ul class="script">
-    {#each zootopiaScript.script as item, index}
+    {#each zootopiaScript as item, index}
     <li class="script__line">
+      {#if 
+        zootopiaScript[index - 1]?.name !== item.name
+      }
       <span class="line__name">{item.name}</span>
+      {/if}
       <p class="line__text">{item.line}</p>
     </li>
     {/each}
   </ul>
+  <button on:click={() => movePage(1)}>1</button>
+  <button on:click={() => movePage(2)}>2</button>
 </div>
 
 <script>
   
   import { getScript } from "../../service/zootopia";
 
-  const zootopiaScript = getScript();
-  
+  let zootopiaScript = getScript();
+
 
   console.log(zootopiaScript)
+
+  function movePage(page) {
+    zootopiaScript = getScript(page);
+  }
 
 </script>
 
@@ -40,13 +50,15 @@
   }
   .script__line {
     margin-bottom: 24px;
-    color: #f0f0f0
+    color: #999
   }
   .line__name {
+    display: block;
     text-transform: uppercase;
     font-weight: bold;
   }
   .line__text {
+    padding-left: 8px;
     padding-top: 4px;
     font-size: 18px;
     line-height: 1.5;
--- src/service/zootopia.js
+++ src/service/zootopia.js
@@ -1,5 +1,11 @@
 import zootopia from "../json/zootopia.json";
 
-export function getScript() {
-  return zootopia;
+export function getScript(page = 1) {
+  const script = [...zootopia.script];
+  const offset = (page - 1) * 10;
+  const limit = 10;
+  console.log(page, offset);
+  const result = script.splice(offset, limit);
+  console.log(result);
+  return result;
 }
